d869881f59318f5c935e9392ded3b3ffdbca6a18
BlushedNanis <luis_danieltg@outlook.com>
2024-02-28 03:21:43 -0500

    Exctract topics and pages from csv to generate the pdf template

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from fpdf import FPDF
 from pandas import read_csv
 
 
-
+df = read_csv('topics.csv')
 
 pdf = FPDF()
 
-
-pdf.add_page()
-pdf.set_font('Arial', 'B', 16)
-pdf.set_text_color(100, 100, 100)
-pdf.cell(0, 16, 'hello there!',0, 1,'L')
-
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 16)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(0, 16, row['Topic'],0, 1,'L')
+    pdf.line(10, 21, 200, 21)
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
     
 
 
