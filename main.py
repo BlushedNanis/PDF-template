a4336e9b935c14f87b648fce5d276b4e84b98631
BlushedNanis <luis_danieltg@outlook.com>
2024-02-28 22:53:11 -0500

    Added inner lines for every page

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(0, 16, row['Topic'],0, 1,'L')
     pdf.line(10, 21, 200, 21)
+    x = 21 #Variable for control of lines
+    #Set inner lines in page
+    for i in range(26):
+        x += 10
+        pdf.line(10, x, 200, x)
     #Set the footer
     pdf.ln(255)
     pdf.set_font('Arial', 'B', 10)
@@ -21,6 +26,11 @@ for index, row in df.iterrows():
     pdf.cell(0, 10, row['Topic'],0, 0,'R')
     for i in range(row['Pages'] - 1):
         pdf.add_page()
+        x = 11 #Variable for control of lines
+        #Set inner lines in page
+        for i in range(27):
+            x += 10
+            pdf.line(10, x, 200, x)
         #Set the footer
         pdf.ln(270)
         pdf.set_font('Arial', 'B', 10)
